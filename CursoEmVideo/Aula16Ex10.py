9379760cd7221c9a2515e42d728a848522e659f8
mlugard <celocabral3@hotmail.com>
2023-01-23 14:01:10 -0300

    Atualizando exercícios de Python

---
+++ CursoEmVideo/Aula16Ex10.py
@@ -0,0 +1,6 @@
+# Exercício
+# 10 - Desenvolva um programa que leia quatro valores pelo teclado e guarde-os numa tupla. No final mostre:
+# A) Quantas vezes apareceu o valor 9;
+# B) Em que posição foi digitado o primeiro valor 3;
+# C) Quais foram os números pares.
+
---
+++ CursoEmVideo/Aula16Ex11.py
@@ -0,0 +1,2 @@
+# Exercício
+# 11 - Crie um programa que tenha uma tupla única com nomes de produtos e seus respectivos preços na sequência. No final, mostre uma listagem de preços, organizando os dados em forma tabular.
