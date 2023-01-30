fdf0041b0f11e6d2d08bf2530fd56b795d889099
mlugard <celocabral3@hotmail.com>
2023-01-30 18:20:17 -0300

    Adicionando exercício 13 da aula 17 de Python.

---
+++ CursoEmVideo/Aula17Ex13.py
@@ -0,0 +1,15 @@
+# 13 - Faça um programa que leia 5 valores numéricos e guarde-os em uma lista. No final, mostre qual foi o maior e o menor valor digitados e as suas respectivas posições na lista.
+
+
+numeros = []
+contador = 0
+while contador <= 4:
+    n = int(input('Digite um número para adicioná-lo à lista: '))
+    numeros.append(n)
+    contador += 1
+
+print(f'O maior número da lista é: {max(numeros)}')
+print(f'E se encontra na posição: {numeros.index(max(numeros))+1}')
+print(f'O menor número da lista é: {min(numeros)}')
+print(f'E se encontra na posição: {numeros.index(min(numeros))+1}')
+
