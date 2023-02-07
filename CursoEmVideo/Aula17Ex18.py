40721d5d092b5fe5c7d800404f89b1e1d4a73a19
mlugard <celocabral3@hotmail.com>
2023-02-07 09:01:31 -0300

    Adicionando exercício 18 da aula 17.

---
+++ CursoEmVideo/Aula17Ex18.py
@@ -0,0 +1,18 @@
+# 18 - Crie um programa onde o usuário digite uma expressão qualquer que use *parênteses*.Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta.
+
+def verifica_expressao(expressao):
+  pilha = []
+  for char in expressao:
+    if char == '(':
+      pilha.append(char)
+    elif char == ')':
+      if not pilha:
+        return False
+      pilha.pop()
+  return not pilha
+
+expressao = input("Digite a expressão: ")
+if verifica_expressao(expressao):
+  print("A expressão está correta.")
+else:
+  print("A expressão está incorreta.")
