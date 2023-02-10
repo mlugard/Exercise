6e1a161ce5d75c8252e3b391d2a003ed6a3420dc
mlugard <celocabral3@hotmail.com>
2023-02-10 13:58:56 -0300

    Melhoria e correção do professor do exercício 23 da aula 18.

--- CursoEmVideo/Aula18Ex23.py
+++ CursoEmVideo/Aula18Ex23.py
@@ -7,17 +7,58 @@
 # 3 - Adicionar as listas de jogo em uma maior contendo todos.
 
 from random import randint
+from time import sleep
+
+print('-='*20)
+print('          JOGUE NA MEGA SENA       ')
+print('-='*20)
 
 qtd_jogos = int(input('Quantos jogos deseja fazer? '))
 jogos = []
 
-palpites_jogos = []
+palpites_jogo = []
 
 while len(jogos) < qtd_jogos:
     for j in range(0, 6):
-        palpites_jogos.append(randint(1, 60))
-    jogos.append(palpites_jogos[:])
-    palpites_jogos.clear()
-print(jogos)
+        num = randint(1, 60)
+        if num not in palpites_jogo:
+            palpites_jogo.append(num)
+        else:
+            num = randint(1, 60)
+            palpites_jogo.append(num)
+    palpites_jogo.sort()
+    jogos.append(palpites_jogo[:])
+    palpites_jogo.clear()
+
+print('-=' * 3, f'SORTEANDO {qtd_jogos} JOGOS', '-=' * 3)
+for i, l in enumerate(jogos):
+    print(f'Jogo {i+1}: {l}')
+    sleep(1)
+print('-=' * 5, 'BOA SORTE! ', '-=' * 5)
 
 
+""" lista = []
+jogos = []
+print('-' * 30)
+print(' JOGA NA MEGA SENA ')
+print('-' * 30)
+quant = int(input('Quantos jogos você quer que eu sorteie? '))
+tot = 1
+while tot <= quant:
+    cont = 0
+    while True:
+        num = randint(1,60)
+        if num not in lista:
+            lista.append(num)
+            cont += 1
+        if cont >= 6:
+            break
+    lista.sort()
+    jogos.append(lista[:])
+    lista.clear()
+    tot += 1
+print('-=' * 3, f'SORTEANDO {quant} JOGOS ', '-=' * 3)
+for i, l in enumerate(jogos):
+    print(f'Jogo {i+1}: {l}')
+    sleep(1)
+print('-=' * 5, 'BOA SORTE! ', '-=' * 5)"""
