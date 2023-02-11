8560c84fc8fff3b0ea8609946b87d23a167cc087
mlugard <celocabral3@hotmail.com>
2023-02-11 19:07:10 -0300

    Adicionando correção e molhoria do exercício 24 da aula 18.

--- CursoEmVideo/Aula18Ex24.py
+++ CursoEmVideo/Aula18Ex24.py
@@ -3,27 +3,31 @@
 # 24 - Crie um programa que leia nome e duas notas de vários alunos e guarde tudo em uma lista composta. No final, mostre um boletim contendo a média de cada um e permita que o usuário possa mostrar as notas de cada aluno individualmente.
 
 alunos = []
-dados_aluno = []
 
 while True:
-    dados_aluno.append(str(input('Digite o nome do aluno: ')))
-    dados_aluno.append(float(input('Nota 1: ')))
-    dados_aluno.append(float(input('Nota 2: ')))
-    media_aluno = (dados_aluno[1] + dados_aluno[2]) / 2
-    dados_aluno.append(media_aluno)
-    alunos.append(dados_aluno[:])
-    dados_aluno.clear()
+    nomeAluno = (str(input('Digite o nome do aluno: ')))
+    nota1 = float(input('Nota 1: '))
+    nota2 = float(input('Nota 2: '))
+    media_aluno = (nota1 + nota2) / 2
+
+    alunos.append([nomeAluno, [nota1, nota2], media_aluno])
 
     continuar = str(input('Deseja continuar? [S/N]'))
     if continuar in 'Nn':
-        for a in alunos:
-            print(f'Número do aluno: {alunos.index(a)}     Nome: {a[0]}    Média: {a[3]}')
-        """ while True: 
-            checar_notasAluno = str(input('Deseja analisar as notas de algum aluno? [S/N]'))
-            if checar_notasAluno in 'Ss':
-                buscar_aluno = str(input('Qual o nome do aluno? '))
-                if buscar_aluno in alunos:
-                    print(f'Nota 1: {alunos[1]}   Nota 2: {alunos[2]}')
-            else: """
         break
-    
+print('-='*30)
+print(f'{"Nº: ":<4}{"Nome: ":<15}{"Média":>8}')
+print('-'*30)
+for i, a in enumerate(alunos):
+    print(f'{i:<4}{a[0]:<15}{a[2]:>8.2f}')
+
+while True: 
+    print('-='*30)
+    buscar_aluno = int(input('Mostrar as notas de qual aluno? (999 interrompe): '))      
+    if buscar_aluno == 999:
+        print('ATÉ BREVE!')
+        break
+    if buscar_aluno <= len(alunos) - 1:
+        print(f'Notas de {alunos[buscar_aluno][0]} são: {alunos[buscar_aluno][1]}')
+
+            
